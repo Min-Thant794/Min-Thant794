8537847b7d0a23e6a20028900f08273394953286
Min-Thant794 <minthanthtun29@gmail.com>
2026-04-08 18:52:30 +0800

    add base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/',
+  base: '/Min-Thant794',
   server: {
     port: 8080
   }
