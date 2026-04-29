fbb2be4fcc0661fcd344bc6a9c41adcf2c2f8f02
Min-Thant794 <minthanthtun29@gmail.com>
2026-04-29 16:19:26 +0800

    removed a section in skills page

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -1,6 +1,5 @@
 import HeroHeader from "../features/skills/sections/HeroHeader"
 import SkillsGrid from "../features/skills/sections/SkillsGrid"
-import SkillsActionSection from "../features/skills/sections/SkillsActionSection"
 
 const Skills = () => {
   return (
@@ -9,7 +8,6 @@ const Skills = () => {
       <main className="mx-auto max-w-7xl overflow-hidden px-5 pb-16 pt-28 sm:px-6 md:pt-32 lg:px-8">
         <HeroHeader />
         <SkillsGrid />
-        <SkillsActionSection />
       </main>
     </div>
   )
